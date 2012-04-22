11f37aafb65aeae97ca4db4697aa61cc4b13fc3c
Ryan Graves <graves.ryan@gmail.com>
2012-04-23 01:45:02 +0200

    adding @uber twitter search

--- main.rb
+++ main.rb
@@ -5,3 +5,7 @@ require 'twitter'
 get '/' do
   Twitter.user_timeline("ryangraves").first.text
 end
+
+Twitter.search("@uber", :rpp => 3, :result_type => "recent").map do |status|
+  "#{status.from_user}: #{status.text}"
+end
