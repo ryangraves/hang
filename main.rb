d626cec037f2b51b3b2a006ff32b3ad363ba26c8
Ryan Graves <graves.ryan@gmail.com>
2012-04-23 02:11:11 +0200

    fixing last save on main

--- main.rb
+++ main.rb
@@ -5,7 +5,3 @@ require 'twitter'
 get '/' do
   Twitter.user_timeline("ryangraves").first.text
 end
-
-Twitter.search("@uber", :rpp => 3, :result_type => "recent").map do |status|
-  "#{status.from_user}: #{status.text}"
-end
